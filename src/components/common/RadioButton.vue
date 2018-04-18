3545865fc551f5cb9f37921833f902f7b3617aad
Donovan Roubos <hello@donovanroubos.nl>
2018-04-18 12:13:05 +0200

    Add RadioButton component file

---
+++ src/components/common/RadioButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="radio-button">
+    <input type="radio" value="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'radio-button',
+}
+</script>
+
+<style lang="scss">
+@import '~$styles/app';
+
+
+</style>
