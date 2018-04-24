b37c6376b7a7cf76135c4776ec889169c58c1e50
Donovan Roubos <hello@donovanroubos.nl>
2018-04-24 22:20:19 +0200

    Edit Stylekit page and removed components from Home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,21 +1,13 @@
 <template>
   <div class="home">
-    <h1>{{ msg }}</h1>
-    <Icon :width="8" :height="8" name="delete"/>
-    <br/>
-    <Heading tag="h1" text="moet je heading 1?"/>
-    <Heading tag="h2" text="moet je heading 2?"/>
-    <Heading tag="h3" text="moet je heading 3?"/>
-    <br>
-    <BodyText text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+    <Logo/>
+    <Heading tag="h1" :text="msg"/>
   </div>
 </template>
 
 <script>
 import Logo from '@/components/Logo';
-import Icon from '@/components/common/Icon';
 import Heading from '@/components/common/Heading';
-import BodyText from '@/components/common/BodyText';
 
 export default {
   name: 'Home',
@@ -24,10 +16,8 @@ export default {
   }),
   components: {
     Logo,
-    Icon,
-    Heading,
-    BodyText,
-  },
+    Heading
+  }
 };
 </script>
 
--- src/pages/Stylekit.vue
+++ src/pages/Stylekit.vue
@@ -24,12 +24,29 @@
       <!-- Radio button -->
       <div class="component">
         <div class="component-name">Button</div>
-          <Button :onClick="buttonClick" type="button" styling="secondary"/>
+          <Button :onClick="buttonClick" type="button" styling="primary"/>
       </div>
 
       <div class="component">
         <div class="component-name">Input Text</div>
         <InputText placeholder="mail@example.com"/>
+      </div>
+
+      <div class="component">
+        <div class="component-name">Icon</div>
+        <Icon :width="8" :height="8" name="delete"/>
+      </div>
+
+      <div class="component">
+        <div class="component-name">Heading</div>
+        <Heading tag="h1" text="moet je heading 1?"/>
+        <Heading tag="h2" text="moet je heading 2?"/>
+        <Heading tag="h3" text="moet je heading 3?"/>
+      </div>
+
+      <div class="component">
+        <div class="component-name">BodyText</div>
+        <BodyText text="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
       </div>
 
     <h2>Custom</h2>
@@ -42,6 +59,10 @@ import Checkbox from '@/components/common/Checkbox';
 import InputText from '@/components/common/InputText';
 import RadioButton from '@/components/common/RadioButton';
 import Button from '@/components/common/Button';
+import Logo from '@/components/Logo';
+import Icon from '@/components/common/Icon';
+import Heading from '@/components/common/Heading';
+import BodyText from '@/components/common/BodyText';
 
 export default {
   name: 'Stylekit',
@@ -50,6 +71,9 @@ export default {
     InputText,
     RadioButton,
     Button,
+    Icon,
+    Heading,
+    BodyText
   },
   methods: {
     buttonClick() {
