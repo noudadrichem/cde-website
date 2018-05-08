6d37edd300cd79f59c82eccc4e9add312fe25966
info@noudadrichem.com <info@noudadrichem.com>
2018-05-08 17:52:36 +0200

    drieluik animations

--- src/components/custom/DrieLuik.vue
+++ src/components/custom/DrieLuik.vue
@@ -1,14 +1,22 @@
 <template>
-<div class="drieluik">
+<div class="drieluik" ref="drieluik">
   <div class=" container-l">
     <div class="row">
 
-      <div class="col-4 luik" v-for="luik in luiken">
+      <div class="col-4 luik to-animate" ref="luik" v-for="(luik, idx) in luiken" :key="idx">
         <img :src="luikUrl(luik.imageName)" alt="luik image" draggable="false">
-        <div class="content">
+
+        <div :class="['content', {active: luikShown[idx]}]">
           <Heading tag="h1" :text="luik.heading"/>
           <BodyText :text="luik.bodyText"/>
-          <a class="read-more" :href="luik.readMoreLink">Lees meer</a>
+
+          <transition name="fade-height">
+            <div v-show="luikShown[idx]">
+              <br/>
+              <BodyText :text="luik.bodyText"/>
+            </div>
+          </transition/>
+          <a @click.prevent="setLuikActive(idx)" class="read-more">Lees meer</a>
         </div>
       </div>
 
@@ -24,6 +32,7 @@ import BodyText from '@/components/common/BodyText'
 
 export default {
   data: () => ({
+    luikShown: [false, false, false],
     luiken: [{
       imageName: 'food',
       heading: 'Food',
@@ -43,8 +52,36 @@ export default {
   }),
   methods: {
     luikUrl(imageName) {
-      return require(`@/assets/images/${imageName}.png`);
+      return require(`@/assets/images/${imageName}.png`)
     },
+    setLuikActive(luikIndex) {
+      const toShowOrNotToShow = !this.luikShown[luikIndex]
+
+      this.luikShown.forEach((isShown, idx) => {
+        if (idx === luikIndex) {
+          this.$set(this.luikShown, luikIndex, toShowOrNotToShow)
+        } else {
+          this.$set(this.luikShown, idx, false)
+        }
+      })
+    },
+    showLuikWhenInFold() {
+      const drieluikIsInScreen = this.$refs.drieluik.getBoundingClientRect().y - (window.innerHeight - 100)
+      console.log({
+        message: 'showLuikWhenInFold',
+        drieluikIsInScreen,
+      });
+      if(drieluikIsInScreen < 0) {
+        this.$refs.luik.forEach((luikNode, idx) => {
+          setTimeout(() => {
+            luikNode.classList.add('fade-in-up')
+          },idx * 220)
+        })
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener('scroll', this.showLuikWhenInFold)
   },
   components: {
     Heading,
@@ -54,6 +91,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$animationTime: 420ms ease;
+
 .drieluik {
   margin-top: 180px;
   position: relative;
@@ -62,6 +101,7 @@ export default {
     background: #C6D1E0;
     height: 500px;
     padding: 0;
+    box-sizing: border-box;
 
     img {
       width: 100%;
@@ -78,6 +118,13 @@ export default {
       transform: translateY(-80px);
       margin: 0 24px;
       padding: 32px;
+      transition: $animationTime;
+
+      &.active {
+        margin: 0;
+        padding: 32px 56px;
+        transform: translateY(-50%);
+      }
 
       .read-more {
         margin-top: 8px;
@@ -88,6 +135,7 @@ export default {
         border-bottom: 1px solid #9c9ba1;
         text-decoration: none;
         display: inline-block;
+        cursor: pointer;
       }
     }
   }
@@ -100,6 +148,19 @@ export default {
     position: absolute;
     top: 500px;
     z-index: -1;
+  }
+
+  .fade-height-enter-active,
+  .fade-height-leave {
+    transition: $animationTime;
+    opacity: 1;
+    max-height: 220px;
+  }
+  .fade-height-enter,
+  .fade-height-leave-to {
+    transition: $animationTime;
+    opacity: 0;
+    max-height: 0;
   }
 }
 </style>
