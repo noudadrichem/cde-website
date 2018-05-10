a98f9f7ab2f7672e07447dd24e33968c8dfce78e
Donovan Roubos <hello@donovanroubos.nl>
2018-05-10 14:56:14 +0200

    Add info icon with info modal on hover

--- src/components/custom/Recipe.vue
+++ src/components/custom/Recipe.vue
@@ -7,7 +7,12 @@
       <div class="counter-container">
         <span class="counter">{{ counter }}ml</span>
         <span class="max-total">/ 700ml</span>
-        <Icon :width="16" :height="16" name="info"/>
+        <div class="info-container">
+          <Icon :width="16" :height="16" name="info"/>
+          <div class="info-modal">
+            Aantal mililiters die in het cocktail glas gaan.
+          </div>
+        </div>
       </div>
       <ul class="ingredients-list">
         <li>35ml Crazy Dutch Dry Gin</li>
@@ -77,6 +82,56 @@ export default {
         color: $template-color-black-lighter;
         margin: 0 8px 0 3px;
       }
+
+      .info-container {
+        position: relative;
+
+        .info-modal {
+          width: 96px;
+          position: absolute;
+          padding: 8px 12px;
+          font-size: 10px;
+          line-height: 14px;
+          background: $template-color-white-default;
+          box-shadow: 0 1px 8px 0 rgba(#CCD1DB, 0.7);
+          left: -52px;
+          top: -68px;
+          opacity: 0;
+          visibility: hidden;
+          transition: all .2s ease-in-out;
+
+          &::after, &::before {
+            content: '';
+            width: 12px;
+            height: 12px;
+            transform: rotate(50deg) skewX(12deg);
+            background: $template-color-white-default;
+            border-radius: 4px;
+            display: block;
+            margin: 0 auto;
+            position: absolute;
+            bottom: -4px;
+            left: calc(50% - 6px);
+            z-index: -1;
+          }
+
+          &::after {
+            z-index: -1;
+            box-shadow: 0 1px 8px 0 rgba(#CCD1DB, 0.7);
+          }
+
+          &::before {
+            z-index: 1;
+            top: 50px;
+          }
+        }
+
+        .icon:hover + .info-modal {
+          visibility: visible;
+          opacity: 1;
+          top: -72px;
+        }
+      }
     }
 
     .ingredients-list {
@@ -86,4 +141,5 @@ export default {
     }
   }
 }
+
 </style>
