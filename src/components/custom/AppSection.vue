b4004697e91c2b68a50f37dbca05f616ad335fb1
Donovan Roubos <hello@donovanroubos.nl>
2018-05-28 11:52:01 +0200

    Add AppSection component

---
+++ src/components/custom/AppSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app">
+    <Dropdown :data="categories" />
+
+  </div>
+</template>
+
+<script>
+import Dropdown from '@/components/custom/Dropdown'
+
+export default {
+  data: () => ({
+    catgories: []
+  }),
+  components: {
+    Dropdown
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
