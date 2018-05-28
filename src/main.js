7f375651a8ce153f6b9d9f4da62a41256c17a0c8
info@noudadrichem.com <info@noudadrichem.com>
2018-05-28 11:34:53 +0200

    http interceptor for token

--- src/main.js
+++ src/main.js
@@ -8,10 +8,20 @@ Vue.use(vueVimeoPlayer)
 Vue.use(resource)
 Vue.config.productionTip = false
 
+if (localStorage.getItem('token')) {
+  Vue.http.interceptors.push((request, next) => {
+    request.headers.set('Authorization', `Bearer ${localStorage.getItem('token')}`)
+    request.headers.set('Accept', 'application/json')
+    next()
+  })
+}
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
