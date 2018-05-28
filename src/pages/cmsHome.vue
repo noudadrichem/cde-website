25d1d05a4c12c35af4f5ca234e2079d2009c28f7
info@noudadrichem.com <info@noudadrichem.com>
2018-05-28 12:21:39 +0200

    poging tot dynamische content rendering

--- src/pages/cmsHome.vue
+++ src/pages/cmsHome.vue
@@ -1,9 +1,41 @@
 <template>
-  <div class="container">
-    <div class="choose-page">
-      <button v-for="pageId in pages" @click="choosePage(pageId)">Home page</button>
+<div class="container">
+  <div class="choose-page">
+    <button v-for="pageId in pages" @click="choosePage(pageId)">Home page</button>
+
+    <div v-if="isSet">
+      <section class="section" v-for="sectionKey in sectionKeys">
+        <div>
+          <div v-for="(value, key, idx) in $data[sectionKey]">
+
+            <div v-if="typeof value === 'object'">
+
+              <div v-for="(value, key, jdx) in value">
+
+                <div v-if="typeof value === 'object'">
+                  <div v-for="(value, key, jdx) in value">
+                    {{ key }} : {{ value }}
+                  </div>
+                </div>
+
+                <div v-else>
+                  {{ key }} : {{ value }}
+                </div>
+              </div>
+            </div>
+
+            <div v-else>
+              {{ key }} : {{ value }}
+            </div>
+
+          </div>
+        </div>
+        <br><br>
+      </section>
     </div>
+
   </div>
+</div>
 </template>
 
 <script>
@@ -13,12 +45,13 @@ export default {
   data: () => ({
     pages: ['5afc46024a04c38c80d4fca0'],
     selectedPageId: '',
-    sections: [],
+    sectionKeys: [],
     heading: {},
     video: {},
     infoSection: {},
     drieluik: {},
     footer: {},
+    isSet: false
   }),
   methods: {
     choosePage(pageId) {
@@ -35,7 +68,10 @@ export default {
     selectedPageId(pageId) {
       this.$http.get(`${conf.apiUrl}content/${pageId}/sections`)
         .then(res => {
-          this.$set(this, 'sections', res.body.sections)
+          const sectionObjects = res.body.sections.map(({
+            title
+          }) => title)
+          this.$set(this, 'sectionKeys', sectionObjects)
 
           const findSectionData = (sectionName, data) => data.body.sections.find(obj => obj.title === sectionName).contents
 
@@ -44,6 +80,7 @@ export default {
           this.$set(this, 'infoSection', findSectionData('infoSection', res))
           this.$set(this, 'drieluik', findSectionData('drieluik', res))
           this.$set(this, 'footer', findSectionData('footer', res))
+          this.$set(this, 'isSet', true)
         })
     }
   },
@@ -53,4 +90,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.section {
+    width: 100%;
+}
+</style>
